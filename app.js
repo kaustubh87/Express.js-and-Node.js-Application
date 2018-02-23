ee7745814a08ea5514d7202afbbd7fc316e73d6e
kvin1087 <kaustubh87@gmail.com>
2018-02-23 16:01:18 -0600

    Created an instance of express and started the server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var express = require('express');
+
+var app = express();
+var port = 4000 || process.env.port;
+
+app.listen(port, function(req,res){
+    console.log('Server running at port ' +port);
+});
