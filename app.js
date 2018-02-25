b0e878847607c90b031cb201145d6db06f882359
kvin1087 <kaustubh87@gmail.com>
2018-02-24 18:17:15 -0600

    Parsed Url function

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var url = require('url');
+var parsedUrl = url.parse("http://www.example.com/profile?name=barry");
 var app = express();
 var port = 4000 || process.env.port;
 
 app.listen(port, function(req,res){
     console.log('Server running at port ' +port);
+    console.log('The parsed url is '+ parsedUrl.protocol + " The host is " +parsedUrl.host+ " The query you entered is " +parsedUrl.query);
+
 });
