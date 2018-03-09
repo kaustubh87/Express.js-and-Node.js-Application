f10741047ee8b5bcbe8d4ccd469612d6a07ddcc6
kvin1087 <kaustubh87@gmail.com>
2018-03-08 22:44:46 -0600

    Added events in node

--- node/events.js
+++ node/events.js
@@ -4,5 +4,5 @@ const events = require('events');
 let emitter = new events.EventEmitter();
 
 emitter.on('newEvent', (message) => {
-    console.log(`Message: ` ${message});
+    console.log(`Message: ` {message});
 });
