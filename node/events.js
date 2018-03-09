2811f321458d88ab081e1089ab0bcf1306f74554
kvin1087 <kaustubh87@gmail.com>
2018-03-08 22:47:37 -0600

    Created new event

--- node/events.js
+++ node/events.js
@@ -4,5 +4,7 @@ const events = require('events');
 let emitter = new events.EventEmitter();
 
 emitter.on('newEvent', (message) => {
-    console.log(`Message: ` {message});
+    console.log(`Message: , ${message}`);
 });
+
+emitter.emit('newEvent', 'Hello This is Kvin');
