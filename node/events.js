6fbd062db380173eee2452373d12a65729c5c037
kvin1087 <kaustubh87@gmail.com>
2018-03-22 23:27:57 -0500

    Added new event listener

--- node/events.js
+++ node/events.js
@@ -1,11 +1,6 @@
 const events = require('events');
-
-
 let emitter = new events.EventEmitter();
 
 emitter.on('newEvent', (message) => {
     console.log(`Message: , ${message}`);
 });
-
-emitter.emit('newEvent', 'Hello This is Kvin');
-emitter.emit('Event 2', 'Hello');
