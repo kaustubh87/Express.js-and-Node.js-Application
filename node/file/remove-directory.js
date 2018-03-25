7a4fc9e7e5db23acdc985b75ca6edc2d2fbe7470
kvin1087 <kaustubh87@gmail.com>
2018-03-25 01:54:52 -0500

    Remove Directory code

---
+++ node/file/remove-directory.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+/* Method to remove directory */
+//fs.rmdirSync('./newDir');
+
+/* Method to remove file inside a given directory */
+fs.unlinkSync('./newDir/newfile.js');
