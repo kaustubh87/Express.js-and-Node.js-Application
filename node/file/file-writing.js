1dfc2a1428c1dc3ad5ff0e061cd5cda550edcb9b
kvin1087 <kaustubh87@gmail.com>
2018-03-24 23:54:09 -0500

    Added append function

--- node/file/file-writing.js
+++ node/file/file-writing.js
@@ -4,4 +4,9 @@ const fs = require('fs');
 fs.writeFile('data.html', 'This is a text file', 'UTF-8', (err) => {
     if(err) return err;
     console.log('The file has been saved');
-})
+});
+
+fs.appendFile('data.html','Wrote this using append function', 'UTF-8', (err)=>{
+    if(err) return err;
+    console.log('The file has been appended');
+});
